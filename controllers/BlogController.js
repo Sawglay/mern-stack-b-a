a9a816385100929c1d5fc443266259fc4b56ea5e
Sawglay <sawglay2@gmail.com>
2026-09-15 20:26:03 +0700

    Added index with controller from BlogRoutes

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -1,5 +1,13 @@
 const BlogController = {
+  index: async (req, res) => {
+    let blogs = await Blog.find().sort({ createdAt: -1 });
+    console.log(blogs);
 
-}
+    res.render("home", {
+      blogs: blogs,
+      title: "Home",
+    });
+  },
+};
 
 module.exports = BlogController;
--- routes/BlogRoutes.js
+++ routes/BlogRoutes.js
@@ -2,20 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get("/blogs", async (req, res) => {
-  // let blogs = [
-  //   {title : 'Blog Title 1', intro : 'This is blog intro 1'},
-  //   {title : 'Blog Title 2', intro : 'This is blog intro 2'},
-  //   {title : 'Blog Title 3', intro : 'This is blog intro 3'}
-  // ];
-  let blogs = await Blog.find().sort({ createdAt: -1 });
-  console.log(blogs);
-
-  res.render("home", {
-    blogs: blogs,
-    title: "Home",
-  });
-});
+router.get("/blogs",);
 
 router.post("/blogs", async (req, res) => {
   let { title, intro, body } = req.body;
